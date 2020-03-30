e6b27e54fae722f7a1c11c47a5dc2b47b0f8f3ac
Seyed Emad Mousavi <emadmoosavi79@gmail.com>
2020-03-30 20:01:34 +0430

    edited final

---
+++ WpfApp1-6/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+
+namespace WpfApp1
+{
+	public class emad
+	{
+		public static int a=0;
+		public static int b = 0;
+	}
+	
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		
+		public MainWindow()
+		{
+			InitializeComponent(); 
+			PreviewKeyDown += (s, e) => { if (e.Key == Key.Escape) Close(); };
+		}
+
+		private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var combo = (ComboBox)sender;
+			var info = (ComboBoxItem)combo.SelectedValue;
+			MessageBox.Show((string)info.Content);
+		
+
+		}
+
+		private void WrapPanel_Click(object sender, RoutedEventArgs e)
+		{
+			FrameworkElement emad = e.Source as FrameworkElement;
+			switch (emad.Name)
+			{
+				case "Aplly": MessageBox.Show("apply");break;
+				case "Restart": MessageBox.Show("reset"); break;
+				case "Refresh": MessageBox.Show("refresh"); break;
+			}
+		}
+
+		private void show_picture(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog op = new OpenFileDialog();  
+		   op.Title = "Select a picture";
+			op.Filter = "Picture Files|*.jpg;*.png;*.bmp;*.jpeg";
+		  if (op.ShowDialog() == true)  
+		  {  
+			  imgPhoto.Source = new BitmapImage(new Uri(op.FileName));  
+		  } 
+		}
+
+		private void imgPhoto_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			new Uri("MainWindow.xaml");
+		}
+
+		private void Hide(object sender, RoutedEventArgs e)
+		{
+			imgPhoto.Visibility = Visibility.Hidden;
+		}
+
+		private void Refresh(object sender, RoutedEventArgs e)
+		{
+			checkbox1.IsChecked = checkbox2.IsChecked = checkbox3.IsChecked = checkbox4.IsChecked = checkbox5.IsChecked = checkbox6.IsChecked = checkbox7.IsChecked = checkbox8.IsChecked =
+			checkbox9.IsChecked = checkbox10.IsChecked = false;
+
+			Note.Text = null;
+		}
+
+		private void Next_page(object sender, RoutedEventArgs e)
+		{
+			if (tab.SelectedIndex == 1)
+				tab.SelectedIndex = 2;
+			else
+				tab.SelectedIndex = 1;
+		}
+
+		private void clock(object sender, RoutedEventArgs e)
+		{
+			Window1 window1 = new Window1();
+			if (emad.a == 0)
+			{ 
+				window1.Show();
+				emad.a = 1;
+			}
+			
+
+		}
+
+		private void Guide(object sender, RoutedEventArgs e)
+		{
+			Window2 window2 = new Window2();
+			if (emad.b == 0)
+			{
+				window2.Show();
+				emad.b = 1;
+			}
+		}
+
+	}
+}
